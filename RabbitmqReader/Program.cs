df5c5f657d9711a6f36b7139bfe43a0c61c5a950
jrleech <87986926+jrleech@users.noreply.github.com>
2021-08-04 13:00:44 -0600

    Add files via upload

---
+++ RabbitmqReader/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using RabbitMQ.Client;
+
+namespace RabbitMQConsumer
+
+{
+    class Program
+
+    {
+
+        private const string UserName = "guest";
+
+        private const string Password = "guest";
+
+        private const string HostName = "localhost";
+
+        static void Main(string[] args)
+
+        {
+
+            ConnectionFactory connectionFactory = new ConnectionFactory
+
+            {
+
+                HostName = HostName,
+
+                UserName = UserName,
+
+                Password = Password,
+
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var channel = connection.CreateModel();
+ 
+            channel.BasicQos(0, 1, false);
+
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+
+            channel.BasicConsume("demoqueue", false, messageReceiver);
+
+            Console.ReadLine();
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+}
+
---
+++ RabbitmqReader/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SystemNetHttpJsonSamples
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string input { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+       
+    }
+}
